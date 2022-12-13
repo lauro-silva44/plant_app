ed4eb73ed1b93bb0fc41dc6f94130e12757de7ae
lauro <lauro@in8.com.br>
2022-12-13 09:22:45 -0100

    prepared the development enviroment

---
+++ lib/screens/components/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:plant_app/constants.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+      ),
+    );
+  }
+}
